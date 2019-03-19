a8b6a6b4f4c2f0aee4a04d3572b33aa5a4caf2d6
LiLiKazine <lilikazine@gmail.com>
2019-03-19 21:24:19 +0800

    init

---
+++ HirugaoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HirugaoDemo
+//
+//  Created by LiLi Kazine on 2019/3/19.
+//  Copyright © 2019 LiLi Kazine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
